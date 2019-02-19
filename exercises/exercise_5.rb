dec06f7bad38fc1a3ce28096bc0d96d98d7c1115
Romel Tan <romel.tan777@gmail.com>
2019-02-19 21:54:02 +0000

    finsihed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue);
+puts "total revenue #{total_revenue}";
+
+num_stores = Store.count();
+puts "average revenue #{total_revenue / num_stores}";
+
+@rich_stores = Store.all.select { |s| s.annual_revenue > 1_000_000};
+pp @rich_stores;
+puts "Number of stores with more than 1 mm revenue: #{@rich_stores.count()}"
