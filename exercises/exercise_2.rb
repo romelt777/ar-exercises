135db72fce9a63be4b03b28cf1d107c902b21940
Romel Tan <romel.tan777@gmail.com>
2019-02-19 21:33:09 +0000

    finsiehd exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.first;
+pp @store1;
+
+@store2 = Store.second;
+pp @store2;
+
+@store1.name = "Markum";
+
+pp @store1;
